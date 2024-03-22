84eb6112e9d30c32b4c67bc8a8719ecf84edbbd7
AmiraSayedMohamed <amira.sayedza@gmail.com>
2024-03-22 12:43:23 -0700

    add file

---
+++ add_node.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _add_node - add node to the head stack
+ * @hd: head of the stack
+ * @x: new_value
+ * Return: no return
+*/
+void _add_node(stack_t **hd, int x)
+{
+
+	stack_t *n_node, *ax;
+
+	ax = *hd;
+	n_node = malloc(sizeof(stack_t));
+	if (n_node == NULL)
+	{ printf("Error\n");
+		exit(0); }
+	if (ax)
+		ax->prev = n_node;
+	n_node->x = x;
+	n_node->next = *hd;
+	n_node->prev = NULL;
+	*hd = n_node;
+}
---
+++ main.h
@@ -0,0 +1,64 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+
+/**
+ * struct bus_s - variables -args, file, line content
+ * @arg: value
+ * @file: pointer to monty file
+ * @content: line content
+ * @lifi: flag change stack <-> queue
+ * Description: carries values through the program
+ */
+typedef struct bus_s
+{
+	char *arg;
+	FILE *file;
+	char *content;
+	int lifi;
+}  bus_t;
+extern bus_t bus;
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/* add.c */
+void _add_(stack_t **hd, unsigned int c);
+/* add_node */
+void _add_node(stack_t **hd, int x);
+
+#endif /* MAIN_H */
